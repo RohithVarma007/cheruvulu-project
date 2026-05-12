622956928b078fdfa98ef89d7dcb00b85549f4ab
rohith V <rohithvarma1242@gmail.com>
2026-05-12 19:41:22 +0530

    prod Run Settings

--- cheruvulu-back/src/main/java/com/cheruvullu/start/controller/OverviewController.java
+++ cheruvulu-back/src/main/java/com/cheruvullu/start/controller/OverviewController.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/OV")
 // @CrossOrigin(origins = "http://localhost:5173")
-// @CrossOrigin(origins = "https://cheruvulu-project.vercel.app")
+@CrossOrigin(origins = "https://cheruvulu-project.vercel.app")
 @RequiredArgsConstructor
 public class OverviewController {
 
--- cheruvulu-front/src/api/api.jsx
+++ cheruvulu-front/src/api/api.jsx
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8080/";
+// const BASE_URL = "http://localhost:8080/";
+const BASE_URL = "https://cheruvulu-project.vercel.app";
 
 // 🔥 generic API handler
 export const api = async (endpoint, method = "GET", body = null) => {
